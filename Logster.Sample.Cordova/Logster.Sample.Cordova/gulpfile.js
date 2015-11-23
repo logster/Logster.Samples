40130be8b31c979c0142008dd2fd5b0078e9462c
jrummell <john@ctrlaltdevelopment.com>
2015-11-22 23:34:19 -0500

    Added Logster.Samples.Cordova

---
+++ Logster.Sample.Cordova/Logster.Sample.Cordova/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/// <binding ProjectOpened='restore' />
+var gulp = require("gulp"),
+    bower = require("gulp-bower");
+
+gulp.task("default", function() {
+    // place code for your default task here
+    return gulp.src("bower_components/jquery/dist/jquery.min.js")
+        .pipe(gulp.dest("www/scripts"));
+});
+
+gulp.task("restore", function() {
+    return bower();
+});
---
+++ Logster.Sample.Cordova/Logster.Sample.Cordova/www/scripts/index.js
@@ -0,0 +1,55 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    // add the logster api key header
+    $.ajaxSetup({ headers: { "ApiKey": "your-api-key" } });
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+
+        var message = {
+            "Message": "app loaded",
+            "Severity": 2,
+            "Category": "Widget Designer",
+            "Application": "Logster.Samples.Cordova",
+            "User": "john-doe"
+        };
+        $.post("https://api.logster.io/api/Log", message);
+
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+
+        var message = {
+            "Message": "app paused",
+            "Severity": 2,
+            "Category": "Widget Designer",
+            "Application": "Logster.Samples.Cordova",
+            "User": "john-doe"
+        };
+        $.post("https://api.logster.io/api/Log", message);
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+
+        var message = {
+            "Message": "app resumed",
+            "Severity": 2,
+            "Category": "Widget Designer",
+            "Application": "Logster.Samples.Cordova",
+            "User": "john-doe"
+        };
+        $.post("https://api.logster.io/api/Log", message);
+    };
+} )();
